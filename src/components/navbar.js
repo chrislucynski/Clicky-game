20f679af9330cef95e755490f307ec406d7e31c3
Chris Lucynski <chris.lucynski@gmail.com>
2019-11-14 10:51:01 -0700

    added more components, rendering Navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/Navbar.css";
+
+
+function Navbar() {
+    return (
+      <nav className="navbar">
+        <a href="/">Clicky Game!</a>
+      </nav>
+    );
+  }
+  
+  export default Navbar;
