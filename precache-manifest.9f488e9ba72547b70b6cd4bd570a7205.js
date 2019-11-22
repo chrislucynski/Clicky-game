30c9aefbd412a697ab1468ad3f51062ee925f5b6
Chris Lucynski <chris.lucynski@gmail.com>
2019-11-22 15:45:47 -0700

    Updates

---
+++ precache-manifest.9f488e9ba72547b70b6cd4bd570a7205.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7155e6a96b27fe490329de0fa7317f94",
+    "url": "/Clicky-game/index.html"
+  },
+  {
+    "revision": "e281c5a90d8e99cf50d2",
+    "url": "/Clicky-game/static/css/main.dfcae131.chunk.css"
+  },
+  {
+    "revision": "2bb21a429f8e752d3049",
+    "url": "/Clicky-game/static/js/2.bf34ed30.chunk.js"
+  },
+  {
+    "revision": "e281c5a90d8e99cf50d2",
+    "url": "/Clicky-game/static/js/main.59fbfc19.chunk.js"
+  },
+  {
+    "revision": "b94bf70277d7ae33ced8",
+    "url": "/Clicky-game/static/js/runtime-main.6d23abd1.js"
+  },
+  {
+    "revision": "d2fd77347a103bdf44215c1ab20a37d1",
+    "url": "/Clicky-game/static/media/galaxy.d2fd7734.jpg"
+  },
+  {
+    "revision": "4b4420cffe3ba4f13221e529e5b60078",
+    "url": "/Clicky-game/static/media/get_schwifty__a_rick_and_morty_font_by_jonizaak_dajw4u8.4b4420cf.ttf"
+  }
+]);
